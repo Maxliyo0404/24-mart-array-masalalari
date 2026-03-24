65f2689d4f923a3b7a19a86b775caf5d62c1c082
Mahliyo <gasandigital13@gmail.com>
2026-03-25 02:20:54 +0500

    masalalar yakunlandi

--- arry.js
+++ arry.js
@@ -34,8 +34,178 @@
 // 3-masala
 // let nums =123;
 
-let b = nums % 10;
-let a = Math.floor((nums / 10 ) % 10);
-let c = Math.floor(nums / 100 );
-let resultat = ( a * 100) + ( c * 10) + b
-console.log(resultat);
+// let b = nums % 10;
+// let a = Math.floor((nums / 10 ) % 10);
+// let c = Math.floor(nums / 100 );
+// let resultat = ( a * 100) + ( c * 10) + b
+// console.log(resultat);
+//  3-masala
+
+// 1-masala
+
+// let a = 5;
+// let b = 10;
+
+// let S = a * b;
+// let P = 2 * (a +b);
+// console.log(S);
+// console.log(P);
+
+// 2-masala
+// const pi = 3.14;
+// let d = 8;
+// let L = pi* d ;
+// let r = d / 2;
+// let S = pi * Math.pow(r, 2);
+// console.log(L, S);
+
+//  3-masala
+
+//  let a = 5;
+//  let V = Math.pow(a, 2);
+//  let S = 6 * Math.pow(a, 2);
+//  console.log(V, S);
+ 
+// 4-masala
+
+// let a = 2;
+// let b = 4;
+// let c = 6;
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c  + a *c);
+// console.log(V, S);
+ 
+// 5-masala
+
+// let a = 60;
+// let b = 80;
+// let M = ( a +b ) / 2;
+// console.log(M);
+
+// 6-masala
+// let a = 6;
+// let b = 8;             
+// let c = Math.sqrt(a**2 + b**2);
+// let P = a + b + c;
+// console.log(c, P);
+
+// 7-masala
+// const pi = 3;
+// let r1 = 10;
+// let r2 = 2;
+// let S1 = pi * r1;
+// let S2 = pi * r2;
+// let S = pi * (Math.pow(r1, 2)- Math.pow(r2, 2));
+// console.log(S1, S2, S);
+
+// 8-masala
+// let L = 24;
+// const pi = 3;
+// let R = L / (2 * pi);
+// let S = pi * Math.pow(R, 2);
+// console.log(R, S);
+ 
+// 9-masala ??
+//  10-masala
+// let a = 2;
+// let b = 4;
+// let c = 6;
+// let natija = [a, b, c] = [c, a, b];
+// console.log(natija);
+
+// 11-masala
+// let x = 2;
+// let y = 4 * Math.pow((x-3), 6) -7 * Math.pow((x -3), 3 ) + 2;
+// console.log(y);
+//  12-masala
+// let a = 2;
+// let b = Math.pow(a, 3);
+// let c =  Math.pow(a, 6);
+// let d = Math.pow(a, 9);
+// let y = Math.pow(a, 15);
+// console.log(b, c, d, y);
+// 13-masala
+// let Tf = 98;
+// let Tc = (Tf -32) * 5 / 9;
+// let Tg = Math.floor(Tc);
+// console.log(Tc, Tg);
+
+// 14-MASALA
+// let x = 2, A = 10000;
+// let y = 4,  B = 16000;
+
+// let jami = (x * A) + (y * B);
+// console.log(jami);
+
+//  15-MASALA??
+
+// 1-MASALA
+//  let son = 123;
+//  let birlar = Math.floor(son % 10);
+//  let onlar = Math.floor((son / 10) % 10);
+// console.log(birlar, onlar);
+
+// 2-MASALA
+
+// let son = 246;
+// let birlar = Math.floor(son % 10);
+// let onlar = Math.floor((son / 10) % 10);
+// let yuzlar = Math.floor(son / 100);
+
+// console.log(birlar + onlar + yuzlar);
+
+// 3-masala
+// let son = 842;
+// let birlar = Math.floor(son % 10);
+// let onlar = Math.floor((son / 10) % 10);
+// let yuzlar = Math.floor(son / 100);
+// let teskari = (birlar * 100) + (onlar * 10 ) + yuzlar;
+// console.log(teskari);
+
+//  4-masala
+
+// let son = 123;
+// let birlar = Math.floor(son % 10);
+// let onlar = Math.floor((son / 10) % 10);
+// let yuzlar = Math.floor(son / 100);
+// let natija = (onlar * 100) + (yuzlar * 10) + birlar;
+// console.log(natija);
+
+// 5-masala
+
+// let son = 468;
+// let birlar = Math.floor(son % 10);
+// let onlar = Math.floor((son / 10) % 10);
+// let yuzlar = Math.floor(son / 100);
+// let natija =  (yuzlar * 100) + (birlar * 10) + onlar ;
+// console.log(natija);
+
+// 6-masala
+// let son = 98123;
+// let a = Math.floor((son / 1000) % 10) ;
+//  console.log(a);
+ 
+// 7-masala
+
+// let N = 240;
+// let a = Math.floor(N / 60);
+// console.log(a);
+// 8-masala
+// let N = 8000;
+// let a = Math.floor(N / 3600);
+// console.log(a);
+
+//  9-masala
+// let N = 150;
+// let a = Math.floor(N / 60 );
+// let b = N % 60;
+
+// console.log(a, b);
+
+
+// 10 -masala
+
+//  let N = 7800;
+// let a = Math.floor(N / 3600 );
+// let b = N % 3600;
+// console.log(a, b);
