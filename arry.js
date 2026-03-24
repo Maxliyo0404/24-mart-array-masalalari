cf9b6741f2d66c845041eb972f280aac6d0acf22
Mahliyo <gasandigital13@gmail.com>
2026-03-24 23:35:03 +0500

    1-masala yakunlandi

--- arry.js
+++ arry.js
@@ -11,11 +11,11 @@
 // console.log(b);
 //  1-masala
 
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-let S = a * b;
-let P = 2 * (a +b);
-console.log(S);
-console.log(P);
+// let S = a * b;
+// let P = 2 * (a +b);
+// console.log(S);
+// console.log(P);
 
