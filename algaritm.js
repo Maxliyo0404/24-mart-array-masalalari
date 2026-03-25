99ab3f1da00ea82a2cc3e3bdfce792dc04064dee
Mahliyo <gasandigital13@gmail.com>
2026-03-25 11:52:55 +0500

    2-masala

--- algaritm.js
+++ algaritm.js
@@ -4,4 +4,10 @@
 // } else {
 //     alert("manfiy")
 // }
-2-masala
+// 2-masala
+
+let a = +prompt("a sonini kiriting");
+let natija = (a % 2 !== 0) ? alert("A soni toq"); alert("a soni toq emas");
+
+
+
