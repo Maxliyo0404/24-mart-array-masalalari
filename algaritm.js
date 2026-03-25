e4812c788acb204319396e880d0f05e9a774007d
Mahliyo <gasandigital13@gmail.com>
2026-03-25 12:11:24 +0500

    5-masala

--- algaritm.js
+++ algaritm.js
@@ -19,12 +19,14 @@
 //     alert("false")
 // }
 // 4-masala
-let a = +prompt("a sonini kiriting");
-let b = +prompt("b sonini kiriting");
-let c = +prompt("c sonini kiriting");
-if (a <= b && b <= c) {
-    alert("true")
-} else {
-    alert("false")
-}
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// if (a <= b && b <= c) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+// 5-masala
 
