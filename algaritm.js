a4457fc48a7e2b944faeb71caf7a7c35ca166ca6
Mahliyo <gasandigital13@gmail.com>
2026-03-25 11:54:40 +0500

    2-masala yakunlandi

--- algaritm.js
+++ algaritm.js
@@ -6,8 +6,8 @@
 // }
 // 2-masala
 
-let a = +prompt("a sonini kiriting");
-let natija = (a % 2 !== 0) ? alert("A soni toq"); alert("a soni toq emas");
+// let a = +prompt("a sonini kiriting");
+// let natija = (a % 2 !== 0) ? alert("A soni toq") : alert("a soni toq emas");
 
 
 
