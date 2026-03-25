e0b66e4c43b56e59d14f6d516966ee95f5175598
Mahliyo <gasandigital13@gmail.com>
2026-03-25 12:23:41 +0500

    6-masala davomi

--- algaritm.js
+++ algaritm.js
@@ -39,3 +39,11 @@
 // }
 
 // 6-masala
+let a = +prompt("a sonini kiriting");
+ let b = +prompt("b sonini kiriting");
+ let c = +prompt("c sonini kiriting");
+ if (a < 0 || b < 0 || c < 0) {
+    alert("true")
+ } else {
+    alert("false")
+ }
