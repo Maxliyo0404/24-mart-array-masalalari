242c3252c972e31e0da4e16a21d2420a2fad527d
Mahliyo <gasandigital13@gmail.com>
2026-03-25 12:01:12 +0500

    3-masala yakunlandi

--- algaritm.js
+++ algaritm.js
@@ -11,10 +11,10 @@
 
 // 3-masala
 
-let a = +prompt("a sonini kiriting");
-let b = +prompt("b sonini kiriting");
-if (a > 2 && b <= 3) {
-    alert("true")
-} else {
-    alert("false")
-}
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// if (a > 2 && b <= 3) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
